e58600611c8d4204412c8bb4a7516babbab43155
Fredrik Larsson <fredrik@lrsn.se>
2015-12-08 21:52:41 +0100

    adds solution for puzzle 2b

--- 02/02.cpp
+++ 02/02.cpp
@@ -4,6 +4,7 @@ static void
 Solve(input_file Input)
 {
 	int TotalArea = 0;
+	int TotalRibbonLength = 0;
 
 	char *Delim = "x\n";
 	char *Token = strtok(Input.Contents, Delim);
@@ -13,16 +14,24 @@ Solve(input_file Input)
 		int Width = atoi(strtok(0, Delim));
 		int Height = atoi(strtok(0, Delim));
 
-		int LW = Length * Width;
-		int WH = Width * Height;
-		int HL = Height * Length;
-		int MinSide = Min(Min(LW, WH), HL);
-
-		int Area = 2 * LW + 2 * WH + 2 * HL + MinSide;
+		int AreaLW = Length * Width;
+		int AreaWH = Width * Height;
+		int AreaHL = Height * Length;
+		int MinFaceArea = Min(Min(AreaLW, AreaWH), AreaHL);
+		int Area = 2 * AreaLW + 2 * AreaWH + 2 * AreaHL + MinFaceArea;
 		TotalArea += Area;
+
+		int PerimLW = 2 * Length + 2 * Width;
+		int PerimWH = 2 * Width + 2 * Height;
+		int PerimHL = 2 * Height + 2 * Length;
+		int MinFacePerimeter = Min(Min(PerimLW, PerimWH), PerimHL);
+		int Volume = Length * Width * Height;
+		int RibbonLength = MinFacePerimeter + Volume;
+		TotalRibbonLength += RibbonLength;
 
 		Token = strtok(0, Delim);
 	}
 
 	printf("%d\n", TotalArea);
+	printf("%d\n", TotalRibbonLength);
 }
