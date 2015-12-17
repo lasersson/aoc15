ec8ebdba67af4c24376750af734953b316f22fc7
Fredrik Larsson <fredrik@lrsn.se>
2015-12-17 14:28:07 +0100

    adds buildall and runall scripts

---
+++ buildall.bat
@@ -0,0 +1,8 @@
+@echo off
+pushd %~dp0
+for /d %%D in ("0*", "1*", "2*") do (
+	pushd %%D
+	call build.bat
+	popd
+)
+popd
---
+++ runall.bat
@@ -0,0 +1,8 @@
+@echo off
+pushd %~dp0\build
+for %%X in ("*.exe") do (
+	echo puzzle %%~nX:
+	%%X input%%~nX.txt
+	echo.
+)
+popd
