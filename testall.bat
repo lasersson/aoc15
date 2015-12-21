905d2a4b75e6c0f507ddcf75915ad8cce22082af
Fredrik Larsson <fredrik@lrsn.se>
2015-12-21 17:32:03 +0100

    adds testall script

---
+++ testall.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal EnableDelayedExpansion
+pushd %~dp0\build
+for %%X in ("*.exe") do (
+	%%X input%%~nX.txt > tempout.txt
+	set RESULT=Fail
+	fc tempout.txt output%%~nX.txt >NUL
+	if !ERRORLEVEL! == 0 set RESULT=Ok!
+	echo puzzle %%~nX: !RESULT!
+)
+popd
